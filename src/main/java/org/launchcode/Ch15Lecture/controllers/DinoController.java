72fed50e34e4a9807ed1d3fccbfa529df4a5cfaf
Joveth Sampson <jovethpac@yahoo.com>
2023-02-17 17:29:36 -0500

    Refactored controller to use CrudRepository instead of DinoDAta

--- src/main/java/org/launchcode/Ch15Lecture/controllers/DinoController.java
+++ src/main/java/org/launchcode/Ch15Lecture/controllers/DinoController.java
@@ -1,7 +1,9 @@
 package org.launchcode.Ch15Lecture.controllers;
 
 import org.launchcode.Ch15Lecture.data.DinoData;
+import org.launchcode.Ch15Lecture.data.DinosaurRepository;
 import org.launchcode.Ch15Lecture.models.Dinosaur;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -19,11 +21,17 @@ import javax.validation.Valid;
 @RequestMapping("dino")//maps the whole controller to the path
 public class DinoController {
 
+    //Let's create a DinosaurRepository field so that we can access all of the CRUD methods we need to save/read dinosaur data from the database...so we can use.findAll
+    //syntax: private Type name
+    @Autowired  //mandatory to add before privateDinosaurRepository
+    private DinosaurRepository dinosaurRepository; //I can name this variable any name I want like Model model
+
     //this is a method that handles get requests at the /dino path
     @GetMapping
     public String dino(Model model){//dino is any made up name, should match what it wants to display
         //let's pass in a list of ALL the Dinos in our data layer. point to the DinoData under data and reference the METHOD
-        model.addAttribute("allDinos", DinoData.getAllDinos());
+        //model.addAttribute("allDinos", DinoData.getAllDinos()); no longer needed
+        model.addAttribute("allDinos", dinosaurRepository.findAll()); //this works because this is an instance that we can use .findAll on
         return"dino/index";//needs to have both locations
     }
 
@@ -64,6 +72,8 @@ public class DinoController {
         //so that our ThymeLeaf template can render ALL of dinos in the table
        // model.addAttribute("allDinos", DinoData.getAllDinos()); removed because no longer needed after redirect was added
         //it would be redundant to have the other model.addAttribute because redirect takes you to dino method that has its own model.addAttribute
+
+        dinosaurRepository.save(newDinoObj); //if we add a dinosaur via the form, this will now be persistently stored in the database which will maintain the data
 
         //return "dino/index";
         return "redirect:"; //as soon we as finish processing the method above (processAddDinoForm), it will redirect us back to the root path or the index page. it will send a get request over to the dino method which is /dino path
